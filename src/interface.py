585225bf6090dfb1dfce126ea57b6ca3ea3da6f7
Rayan Ahmed <rayan12152007@gmail.com>
2026-03-26 13:52:04 -0400

    Pre Merge

--- src/interface.py
+++ src/interface.py
@@ -1,13 +1,13 @@
 
 
-def cli_input() -> str:
+def get_input_password() -> str:
     """Returns user input text"""
     input_text = input("What password do you want to store?")
 
     return input_text
 
 
-def cli_output(output: str) -> None:
+def display_message(output: str) -> None:
     """Prints output string"""
     print(output)
 
