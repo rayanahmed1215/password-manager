93736de1e69dd9c6d5bdc73b3ed108b7f43937a7
Rayan Ahmed <rayan12152007@gmail.com>
2026-03-26 13:43:02 -0400

    Added CLI input and output functions

---
+++ src/interface.py
@@ -0,0 +1,14 @@
+
+
+def cli_input() -> str:
+    """Returns user input text"""
+    input_text = input("What password do you want to store?")
+
+    return input_text
+
+
+def cli_output(output: str) -> None:
+    """Prints output string"""
+    print(output)
+
+    return
